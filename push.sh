49cc8d0e5c87986cf335d026ab282ba2c1ed7a41
Li-Sansi <849902298@qq.com>
2023-06-08 19:44:02 +0800

    @fangjh21.20230608

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "@fangjh21.20230603" 
+git commit -m "@fangjh21.20230608" 
 git branch -M main 
 git remote add origin git@github.com:Li-Sansi/wafer-sim.git
 git push -u origin main
