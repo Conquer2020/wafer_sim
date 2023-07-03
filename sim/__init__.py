280961a82a162a4f8822c800464400a691acab9a
Li-Sansi <849902298@qq.com>
2023-07-03 21:40:15 +0800

    add analysis model

---
+++ sim/__init__.py
@@ -0,0 +1,15 @@
+
+'''
+import sim.util
+import sim.ML
+import sim.comp_graph
+import sim.model_gen
+import sim.monitored_resource
+import sim.op_pd
+import sim.pipeline
+import sim.tile_dataflow
+import sim.wafer_device
+import sim.main_run
+
+__all__ = ["util", "ML", "comp_graph","util", "model_gen", "monitored_resource","op_pd","pipeline","tile_dataflow","wafer_device","main_run"]
+'''
--- sim/model_gen.py
+++ sim/model_gen.py
@@ -2,16 +2,17 @@ from comp_graph import CompGraph,OpNode
 from ML import *
 
 #here is a sample Tranformer model for test
-L,B,S,H,A=96,1564,2048,12288,96
-ops=[]
-gp=CompGraph()
-for i in range(L):
-    hint='t'+str(i)
-    ops.append(OpNode(op_type=OP.Transformer,op_param=[B,S,H,A],hint_name=hint))
-    #print(i)
-    if i==0:
-        gp.AddEdge(ops[i])
-    else:
-        gp.AddEdge(ops[i],ops[i-1])
-        
-CompGraph.gwrite(gp,path='mljson',name='gpt-3.json')
+#
+def Transformer_Gen(L=96,B=1564,S=2048,H=12288,A=96):
+    #L,B,S,H,A=L,B,S,H,A
+    ops=[]
+    gp=CompGraph()
+    for i in range(L):
+        hint='t'+str(i)
+        ops.append(OpNode(op_type=OP.Transformer,op_param=[B,S,H,A],hint_name=hint))
+        #print(i)
+        if i==0:
+            gp.AddEdge(ops[i])
+        else:
+            gp.AddEdge(ops[i],ops[i-1])
+    CompGraph.gwrite(gp,path='mljson',name='gpt-3.json')
