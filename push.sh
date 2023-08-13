1c2e34672fabba0678bfb550acc896e9aa39b975
Li-Sansi <849902298@qq.com>
2023-08-13 15:21:55 +0800

    add Analytical model for accelerate the training process

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "fix some bugs" 
+git commit -m "add Analytical model for accelerate the training process" 
 git branch -M main 
 git remote add origin git@github.com:Li-Sansi/wafer-sim.git
 git push -u origin main
